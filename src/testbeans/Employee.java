bf38066a4b8477a50ac7192d7d897d9085f4acd3
Nilachal Chakraborty <cnilachal.19@gmail.com>
2020-01-20 02:38:28 +0530

    Added the serialVersionUID for Emplyee.java

--- src/testbeans/Employee.java
+++ src/testbeans/Employee.java
@@ -3,6 +3,7 @@ import java.io.Serializable;
 
 public class Employee implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
 	private String name;
 	private Integer age;
 	private Department department = new Department();
