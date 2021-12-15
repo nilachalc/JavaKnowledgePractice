6f749df2ee3e5b3453bef1ee6e23a615111d09d6
nilachalc <cnilachal.19@gmail.com>
2021-12-16 03:24:51 +0530

    Updated strings

---
+++ src/stringhandling/Practice.java
@@ -0,0 +1,13 @@
+package stringhandling;
+
+public class Practice {
+	public static void main(String args[]) {
+		 String s = "Nilachal";
+		 String s1 = "Nilachal";
+		 System.out.println(s==s1);
+		 s = "Chakraborty";
+		 System.out.println(s==s1);
+		 String s2 = "Nilachal";
+		 System.out.println(s1==s2);
+	 }
+}
