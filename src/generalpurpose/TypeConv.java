e3161d529a2e94e332fcec29b014906e123391aa
nilachalc <cnilachal.19@gmail.com>
2024-05-26 18:49:27 +0530

    Added code for bit wise operator.

--- src/generalpurpose/TypeConv.java
+++ src/generalpurpose/TypeConv.java
@@ -6,5 +6,17 @@ public class TypeConv {
 		System.out.println(b);
 		b*=20;  //Shorthand operators skips the automatic type conversion. 
 		System.out.println(b);
+		
+		int a = -8;
+		System.out.println(Integer.toBinaryString(a));
+		System.out.println(a << 3);
+		System.out.println(Integer.toBinaryString(a << 3));
+		System.out.println(a >> 2);
+		System.out.println(Integer.toBinaryString(a >> 2));
+		System.out.println(a >>> 2);// As the unsigned right shift operator is used so the Higher order bits are being considered as value and not sign
+		System.out.println(Integer.toBinaryString(a >>> 2));
+		int p = 8;
+		System.out.println(p >>> 2);
+		System.out.println(Integer.toBinaryString(p >>> 2));
 	}
 }
