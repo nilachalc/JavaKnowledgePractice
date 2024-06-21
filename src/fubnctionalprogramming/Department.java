f3422c994fab8bc13e49a292dd6e4a3c9bb4a9d6
nilachalc <cnilachal.19@gmail.com>
2024-06-22 02:03:54 +0530

    Added functinal programming code.

---
+++ src/fubnctionalprogramming/Department.java
@@ -0,0 +1,25 @@
+package fubnctionalprogramming;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @AllArgsConstructor @ToString 
+public class Department { 
+	private String id;
+	private String name;
+	private int durationInMonths; 
+	private Integer numberOfStudents;
+	private boolean isRviewAvailable;
+	private String review;
+		
+	public static List<Department> comp(List<Department> allDepartments, Comparator<Department> compareLogic) {
+			return allDepartments.stream().sorted(compareLogic).collect(Collectors.toList());
+	}
+}
+				
---
+++ src/fubnctionalprogramming/PlyingWithStreams.java
@@ -0,0 +1,23 @@
+package fubnctionalprogramming;
+
+import java.math.BigInteger;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+public class PlyingWithStreams {
+	public static void main (String[] args) {
+		System.out.println(IntStream.range(1, 11));
+		System.out.println();
+		System.out.println(IntStream.rangeClosed(1, 10));
+		System.out.println();
+		System.out.println(IntStream.rangeClosed(1, 10). sum());
+		System.out.println(IntStream.iterate(2,	i -> {i = i + 2;return i * i;}).limit(4).peek(i -> System.out.println(i)).average());
+		System.out.println (LongStream.rangeClosed(1, 30)
+				.mapToObj(BigInteger :: valueOf).limit(10)
+				.peek(i -> System.out.println(i))
+				.reduce(BigInteger.ONE, (x, y) -> x.multiply(y)));
+		System.out.println(LongStream.iterate(2, x -> x * x)
+				.mapToObj(BigInteger :: valueOf)
+				.limit(10).peek(bi -> System.out.println(bi.doubleValue())));
+	}
+}
