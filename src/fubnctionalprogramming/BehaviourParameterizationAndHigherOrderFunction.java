a198f1fa3029c61ca937635a8f5c9ddbbc535aab
nilachalc <cnilachal.19@gmail.com>
2024-06-28 01:52:08 +0530

    Updated functional programming code.

---
+++ src/fubnctionalprogramming/BehaviourParameterizationAndHigherOrderFunction.java
@@ -0,0 +1,53 @@
+package fubnctionalprogramming;
+
+import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+
+public class BehaviourParameterizationAndHigherOrderFunction {
+	private static List<Integer> integers = List.of(4, 17, 6, 8, 9, 5, 6, 13, 52, 73); 
+	
+	private static void calcAndPrint(Function<Integer, Integer> fn) {
+		integers.stream().map(fn).forEach(x -> System.out.println(x));
+	}
+	
+	private static void printQube() {
+		System.out.println("Qubes: ");
+		calcAndPrint(x -> x * x * x);
+	}
+	
+	private static void printSqure() {
+		System.out.println("Squre: ");
+		calcAndPrint(x -> x * x);
+	}
+	
+	private static BinaryOperator<Integer> operationDecider(String val) {
+		switch (val) {
+			case "SUM": {
+				return (x, y) -> x + y;
+			}
+			case "SUBTRACT": {
+				return (x, y) -> x - y;
+			}
+			case "MULTIPLY": {
+				return (x, y) -> x * y;
+			}
+			case "DEVIDE": {
+				return (x, y) -> x / y;
+			}
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + val);
+		}
+	}
+	
+	private static void calc(BinaryOperator<Integer> bf, Integer i, Integer j) {
+		System.out.println(bf.apply(i, j));
+	}
+	
+	public static void main(String[] args) {
+		printSqure();
+		printQube();
+		
+		calc(operationDecider("MULTIPLY"), 5, 6);
+	}
+}
---
+++ src/fubnctionalprogramming/JoinAndFlapmap.java
@@ -0,0 +1,16 @@
+package fubnctionalprogramming;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class JoinAndFlapmap {
+	
+	public static void main(String[] args) {
+		List<String> courses = List.of("Spring", "Spring Boot", "API" , "Microservices","AWS", "PCF","Azure", "Docker", "Kubernetes");
+		System.out.println(courses.stream().collect(Collectors.joining(".")));
+		
+		System.out.println(courses.stream().map(course -> course.split("")).flatMap(Arrays :: stream).distinct().collect(Collectors.toList()));
+	}
+}
+
