4cd86e2309a7ccd8a7cb668bc43af5b974821225
nilachalc <cnilachal.19@gmail.com>
2025-03-26 17:39:38 -0400

    code updated.

--- src/functionalprogramming/PlyingWithStreams.java
+++ src/functionalprogramming/PlyingWithStreams.java
@@ -6,18 +6,19 @@ import java.util.stream.LongStream;
 
 public class PlyingWithStreams {
 	public static void main (String[] args) {
-		System.out.println(IntStream.range(1, 11));
+		IntStream.range(1, 11).forEach(i -> System.out.print(i + "  "));
 		System.out.println();
-		System.out.println(IntStream.rangeClosed(1, 10));
+		IntStream.rangeClosed(1, 11).forEach(i -> System.out.print(i + "  "));
 		System.out.println();
-		System.out.println(IntStream.rangeClosed(1, 10). sum());
-		System.out.println(IntStream.iterate(2,	i -> {i = i + 2;return i * i;}).limit(4).peek(i -> System.out.println(i)).average());
+		System.out.println(IntStream.rangeClosed(1, 10).sum());
+		System.out.println();
+		System.out.println(IntStream.iterate(1,	i -> {i++ ;return i * i;}).limit(4).peek(i -> System.out.println(i)).average());
 		System.out.println (LongStream.rangeClosed(1, 30)
 				.mapToObj(BigInteger :: valueOf).limit(10)
 				.peek(i -> System.out.println(i))
 				.reduce(BigInteger.ONE, (x, y) -> x.multiply(y)));
-		System.out.println(LongStream.iterate(2, x -> x * x)
+		LongStream.iterate(2, x -> x * x)
 				.mapToObj(BigInteger :: valueOf)
-				.limit(10).peek(bi -> System.out.println(bi.doubleValue())));
+				.limit(10).peek(bi -> System.out.println(bi.doubleValue()));
 	}
 }
